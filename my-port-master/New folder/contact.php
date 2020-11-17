98380427acee094f6134a03673032e66284ed3c9
siphot78 <70804959+siphot78@users.noreply.github.com>
2020-11-17 08:39:33 +0200

    Add files via upload

---
+++ my-port-master/New folder/contact.php
@@ -0,0 +1,260 @@
+
+
+
+
+<html lang="en"><!-- Basic --><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!-- Site Metas -->
+    <title>Codetelligence</title>
+    <meta name="keywords" content="Codetelligent, Codetelligence, coding Bootcamps, coding Bootcamp, khayelitsha coding, learn to code, Keoikantse Tsholofelo Botlholo">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="#" type="image/x-icon">
+    <link rel="apple-touch-icon" href="#">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Pogo Slider CSS -->
+    <link rel="stylesheet" href="css/pogo-slider.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="inner_page" data-spy="scroll" data-target="#navbar-wd" data-offset="98" style="overflow: visible;">
+
+    <!-- LOADER -->
+    <div id="preloader" style="display: none;">
+        <div class="loader">
+            <img src="images/loader.gif" alt="#">
+        </div>
+    </div>
+    <!-- end loader -->
+    <!-- END LOADER -->
+
+    <!-- Start header -->
+    <header class="top-header">
+        <nav class="navbar header-nav navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="image"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-wd" aria-controls="navbar-wd" aria-expanded="false" aria-label="Toggle navigation">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbar-wd">
+                    <ul class="navbar-nav">
+                        <li><a class="nav-link active" href="index.html">Home</a></li>
+                        <li><a class="nav-link" href="about.html">About</a></li>
+						<li><a class="nav-link" href="courses.html">Courses</a></li>
+						<li><a class="nav-link" href="kiddies.html">Kiddies</a></li>
+						<li><a class="nav-link" href="news.html">Cohort</a></li>
+						<li><a class="nav-link" href="contact.html">Contact us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- End header -->
+
+	<!-- section -->
+	
+	<section class="inner_banner">
+	  <div class="container">
+	      <div class="row">
+		      <div class="col-12">
+			     <div class="full">
+				     <h3>Contact us</h3>
+				 </div>
+			  </div>
+		  </div>
+	  </div>
+	</section>
+	
+	<!-- end section -->
+   
+	<!-- section -->
+    <div class="section layout_padding contact_section" style="background:#f6f6f6;">
+        <div class="container">
+               <div class="row">
+                 <div class="col-lg-8 col-md-8 col-sm-12">
+				    <div class="full float-right_img">
+                        <img src="images/img10.jpg" alt="#">
+                    </div>
+                 </div>
+				 <div class="col-lg-4 col-md-4 col-sm-12">
+				    <div class="contact_form">
+					    <form action="mailto:keoikantse@codetelligent.co.za" method="POST" enctype="text/plain">
+						   <fieldset>
+						       <div class="full field">
+							      <input type="text" placeholder="Your Name" name="name" id="name">
+							   </div>
+							   <div class="full field">
+							      <input type="email" placeholder="Email Address" name="Email" id="email" >
+							   </div>
+							   <div class="full field">
+							      <input type="phn" placeholder="Phone Number" name="Phone-number" id=phone-number>
+							   </div>
+							   <div class="full field">
+                               <textarea class="form-control" id="body" placeholder="Email Body"></textarea>
+							   </div>
+							   <div class="full field">
+							      <div class="center"><input type="button" onclick="sendEmail()" value="Send An Email" class="btn btn-primary"></div>
+							   </div>
+						   </fieldset>
+						</form>
+					</div>
+                 </div>
+               </div>			  
+           </div>
+        </div>
+	<!-- end section -->
+    <!-- Start Footer -->
+    <footer class="footer-box">
+        <div class="container">
+		
+		   <div class="row">
+		   
+		      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+				<div class="footer_blog">
+				    <div class="footer_blog footer_menu white_fonts">
+						<h3>Our Model</h3>
+					    <p>Flexible learning </p>
+                       <p>Project-based</p>
+                       <p>Mentor-led</p>
+					 </div>
+				 </div>
+			  </div>
+			  
+			  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+			       <div class="footer_blog footer_menu white_fonts">
+					<h3>Social Media</h3>
+					<ul> 
+					    <li><a href="https://www.facebook.com/CodetelligentAfrica">&gt; Facebook</a></li>
+                        <li><a href="https://twitter.com/Code_Telligent">&gt; Twitter</a></li>
+                        <li><a href="https://www.instagram.com/codetelligent_africa/">&gt; Instagram</a></li>
+                        <li><a href="https://www.linkedin.com/company/codetelligent">&gt; Linkedin</a></li>
+					</ul>
+						 </div>
+				 </div>
+				 
+				 <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+				 <div class="footer_blog full white_fonts">
+						     <h3>Newsletter</h3>
+							 <p>Fill in the form below to receive our monthly newsletter.</p>
+							 <div class="newsletter_form">
+							    <form action="index.html">
+								   <input type="email" placeholder="Your Email" name="#" required="">
+								   <button><a href="mailto:hello@codetelligent.co.za">Submit</a></button>
+								</form>
+							 </div>
+						 </div>
+					</div>	 
+			  
+			  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+				 <div class="footer_blog full white_fonts">
+						     <h3>Contact us</h3>
+							 <ul class="full">
+							   <li><img src="images/i5.png"><span>KCT Show Village<br>Walter Sisulu Drive<br>Cape Town</span></li>
+							   <li><img src="images/i6.png"><span>hello@Codetelligence.org.za</span></li>
+							   <li><img src="images/i7.png"><span>083 609 2704</span></li>
+							 </ul>
+						 </div>
+					</div>	 
+			  
+		   </div>
+		
+        </div>
+    </footer>
+    <!-- End Footer -->
+
+    <div class="footer_bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p class="crp">© Copyrights 2020 design by codetelligent.co.za</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <a href="#" id="scroll-to-top" class="hvr-radial-out"><i class="fa fa-angle-up"></i></a>
+
+    <!-- ALL JS FILES -->
+    <script src="js/jquery.min.js"></script>
+	<script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.pogo-slider.min.js"></script>
+    <script src="js/slider-index.js"></script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/isotope.min.js"></script>
+    <script src="js/images-loded.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+   
+    <script>
+ function sendEmail() {
+            var name = $("#name");
+            var email = $("#email");
+            var phone = $('#Phone-number');
+            var subject = $("#subject");
+            
+            var body = $("#body");
+
+            if (isNotEmpty(name) && isNotEmpty(email) && isNotEmpty(subject) && isNotEmpty(body)) {
+                $.ajax({
+                   url: 'sendEmail.php',
+                   method: 'POST',
+                   dataType: 'json',
+                   data: {
+                       name: name.val(),
+                       email: email.val(),
+                       subject: subject.val(),
+                       body: body.val()
+                   }, success: function (response) {
+                        if (response.status == "success")
+                            alert('Email Has Been Sent!');
+                        else {
+                            alert('Please Try Again!');
+                            console.log(response);
+                        }
+                   }
+                });
+            }
+        }
+
+        function isNotEmpty(caller) {
+            if (caller.val() == "") {
+                caller.css('border', '1px solid red');
+                return false;
+            } else
+                caller.css('border', '');
+
+            return true;
+        }
+
+
+	</script>
+
+
+</body></html>
